916875f04db02746c75ed8ed528b9bc701799b5b
Rahul <rahul222366@gmail.com>
2020-12-07 19:20:07 +0530

    first commit

---
+++ app/src/main/java/com/rahul/shortcut/ClaimActivity.kt
@@ -0,0 +1,11 @@
+package com.rahul.shortcut
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ClaimActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_claim)
+    }
+}
---
+++ app/src/main/java/com/rahul/shortcut/IdCardActivity.kt
@@ -0,0 +1,11 @@
+package com.rahul.shortcut
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IdCardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_id_card)
+    }
+}
